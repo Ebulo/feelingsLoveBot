74ddd9299778becc267c86dd3acf8363790ee820
Bishant <bishant.nayak44@gmail.com>
2022-03-30 00:31:08 +0530

    Created the main function which only creates a output.txt file which would have the output data in future

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import numpy
 
 print("Project Initiated!")
+
+#Here goes the main content
+
+def main():
+	with open("output.txt", "w") as output:
+		output.write("Adding First creation content")
+
+
+if __name__ == "__main__":
+	main()
